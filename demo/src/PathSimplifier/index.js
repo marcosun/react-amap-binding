47ef1ca9e63161ec9545f7603f972e57be897a71
marcosun <marcoinbristol@gmail.com>
2018-03-30 13:56:09 +0800

    pathSimplifier demo

--- demo/src/PathSimplifier/index.js
+++ demo/src/PathSimplifier/index.js
@@ -25,6 +25,50 @@ export default class PathSimplifierPage extends React.Component {
   };
 
   /**
+   * Constructor
+   * @param {Object} props
+   */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pathSimplifier: [{
+        name: '轨迹0',
+        path: [
+          [120.432955, 30.234711],
+          [120.183016, 30.243906],
+          [120.163431, 30.254176],
+        ],
+      }, {
+        name: '轨迹1',
+        path: [
+          [120.177591, 30.217746],
+          [120.215529, 30.250078],
+          [120.207117, 30.276618],
+        ],
+      }],
+    };
+  }
+
+  /**
+   * Test Marker component update functionalities
+   */
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        pathSimplifier: [{
+          name: '轨迹0',
+          path: [
+            [120.239218, 30.235842],
+            [120.181196, 30.264609],
+            [120.167635, 30.246223],
+          ],
+        }],
+      });
+    }, 5000);
+  }
+
+  /**
    * Show AMap with full screen width and height
    * @return {Component} - Page
    */
@@ -37,21 +81,14 @@ export default class PathSimplifierPage extends React.Component {
       <div className={classes.mapContainer}>
         <AMap>
           <PathSimplifier
-            data={[{
-              name: '轨迹0',
-              path: [
-                [120.432955, 30.234711],
-                [120.183016, 30.243906],
-                [120.163431, 30.254176],
-              ],
-            }]}
+            data={this.state.pathSimplifier}
             getPath={(pathData, pathIndex) => {
               return pathData.path;
             }}
             getHoverTitle={() => {
               return null;
             }}
-            autoSetFitView={false}
+            autoSetFitView={true}
             clickToSelectPath={false}
           />
         </AMap>
