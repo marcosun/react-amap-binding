5350c2438e76028ab357b3c28356cddc3a1b4162
Zi Mo <1027089889@qq.com>
2018-04-02 09:09:48 +0800

    InfoWindow demo

---
+++ demo/src/InfoWindow/index.js
@@ -0,0 +1,79 @@
+/**
+ * @module Demo/InfoWindowPage
+ */
+import React from 'react';
+import {object} from 'prop-types';
+import {withStyles} from 'material-ui';
+import {InfoWindow} from 'react-amap-binding';
+
+import AMap from '../AMapPage';
+
+const styles = (theme) => ({
+  mapContainer: {
+    width: '100vw',
+    height: '100vh',
+  },
+});
+
+@withStyles(styles)
+/**
+ * InfoWindowPage page
+ */
+export default class InfoWindowPage extends React.Component {
+  static propTypes = {
+    classes: object,
+  };
+
+  /**
+   * Constructor
+   * @param {Object} props
+   */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      infoWindow: {
+        isCustom: false,
+        position: [120.162692, 30.253647],
+        content: '<div>InfoWindow<br>InfoWindow</div>',
+        autoMove: true,
+        offset: [0, 0],
+        visible: true,
+      },
+    };
+  }
+
+  /**
+   * Click handler.
+   * @param {Object} e - Event
+   */
+  handleClick = (e) => {
+    alert('是否要关闭视窗？');
+  }
+
+  /**
+   * Show InfoWindowPage with full screen width and height
+   * @return {Component} - Page
+   */
+  render() {
+    const {
+      classes,
+    } = this.props;
+
+    const {
+      infoWindow,
+    } = this.state;
+    console.log(...infoWindow);
+    return (
+      <div className={classes.mapContainer}>
+        <AMap>
+          <InfoWindow
+            {...infoWindow}
+            onClose={this.handleClick}
+          />
+        </AMap>
+      </div>
+    );
+  }
+}
+
