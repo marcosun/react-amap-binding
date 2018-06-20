8c88e2470c4c4a755f1fad47a7ca6a17064b95d4
余珩 <1281005689@qq.com>
2018-06-20 11:02:58 +0800

    [demo] Update pathNavigator demo

--- demo/src/PathNavigator/index.js
+++ demo/src/PathNavigator/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {object} from 'prop-types';
 import {withStyles} from 'material-ui';
-import {PathNavigator} from 'react-amap-binding';
+import {PathNavigator, PathSimplifier} from 'react-amap-binding';
 
-import PathSimplifier from '../PathSimplifier';
+import AMap from '../AMapPage';
 
 const styles = (theme) => ({
   mapContainer: {
@@ -27,6 +27,31 @@ class PathNavigatorPage extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
+
+    this.pathSimplifier = [{
+      name: '轨迹0',
+      path: [
+        [120.432955, 30.234711],
+        [120.183016, 30.243906],
+        [120.163431, 30.254176],
+      ],
+    }, {
+      name: '轨迹1',
+      path: [
+        [120.177591, 30.217746],
+        [120.215529, 30.250078],
+        [120.207117, 30.276618],
+      ],
+    }];
+  }
+
+  /**
+   * @param  {Object} map - AMap instance
+   * @param  {Object} target - Current instance
+   * @param  {Object} pathSimplifier - PathSimplifier instance
+   */
+  handleComplete(map, target, pathSimplifier) {
+    target.start();
   }
 
   /**
@@ -39,11 +64,25 @@ class PathNavigatorPage extends React.Component {
 
     return (
       <div className={classes.mapContainer}>
-        <PathSimplifier>
-          <PathNavigator
-            pathIndex={0}
-          />
-        </PathSimplifier>
+        <AMap>
+          <PathSimplifier
+            data={this.pathSimplifier}
+            getPath={(pathData, pathIndex) => {
+              return pathData.path;
+            }}
+            getHoverTitle={() => {
+              return null;
+            }}
+            autoSetFitView={true}
+            clickToSelectPath={false}
+          >
+            <PathNavigator
+              loop={true}
+              onComplete={this.handleComplete.bind(this)}
+              pathIndex={0}
+            />
+          </PathSimplifier>
+        </AMap>
       </div>
     );
   }
