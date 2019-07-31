3ed521dba56d0241c0aa0a4dbed615bd2fa43d69
marcosun <marcoinbristol@gmail.com>
2019-07-31 15:10:43 +0800

    [createEventCallback] Do not expose map instance because map instance is expected to be access via context.

--- src/utils/createEventCallback.js
+++ src/utils/createEventCallback.js
@@ -1,24 +1,27 @@
 /**
  * Event callback factory.
- * By reading and executing callback function from props every time,
- * the event callback is called, changes made to event callbacks will be honoured.
- * By contrast, one may cancel an event listener and attach a new listener if props get updated.
- * @param {string} callbackName - Function name on props
- * @param {Object} target - Target component instance
+ * By reading and executing callback function from props every time the event callback is called,
+ * changes made to event callbacks will be honoured.
+ * However if not implement this way, one may cancel an event listener and attach a new listener
+ * if props get updated.
+ * @param {string} callbackName - Function name on props.
+ * @param {Object} target - Target component instance.
  * @return {Function} - Function to be called as event callback on AMap.
- * Parameters takes the following orders:
- * AMap.Map instance,
- * followed by AMap target component instance,
- * followed by AMap event parameters.
+ * Signature:
+ * (target, ...event) => void
+ * target: AMap target component instance.
+ * event: AMap event.
  */
 export default function(callbackName, target) {
-  return function callCallback(...params) {
+  return function eventCallback(...params) {
     const callback = this.props[callbackName];
-    const map = this.context;
     if (typeof callback === 'function') {
-      map.CLASS_NAME === 'AMap.Map'
-        ? callback(map, target, ...params)
-        : callback(target, ...params);
+      /**
+       * Target is map instance if event is watched in AMap.
+       * Target is circle instance if event is watched in Circle.
+       * Map instance should be access via context.
+       */
+      callback(target, ...params);
     }
   };
 }
