f10fa511135086c1861cf3ce0e38b80aaaedee3a
marcosun <marcoinbristol@gmail.com>
2019-08-02 12:54:36 +0800

    [breakIfNotChildOfAMap] Change comments.

--- src/utils/breakIfNotChildOfAMap.js
+++ src/utils/breakIfNotChildOfAMap.js
@@ -1,13 +1,12 @@
 /**
  * Throw error if a testing module is not a child component of AMap component
- * by testing global variable window.AMap and its created instance
- * @param  {string} moduleName - Name of the testing module
- * @param  {AMap.Map} mapObject - Instance of AMap.Map
- * @param  {string} parentModuleName='AMap' - Name of parent module
+ * by testing the existence of global variable window.AMap and its created map instance.
  */
 export default function(moduleName, mapObject, parentModuleName = 'AMap') {
   if (window.AMap === void 0 || mapObject === void 0) {
-    throw Error(`${moduleName} cannot be used as a standalone. ` +
-      `${moduleName} must be a child component of ${parentModuleName}.`);
+    throw Error(
+      `${moduleName} cannot be used independently. ` +
+      `${moduleName} must be a child component of ${parentModuleName}.`,
+    );
   }
 }
