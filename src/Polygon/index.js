a2d50d886e4e95f35eea86d6756b79a439eef265
marcosun <marcoinbristol@gmail.com>
2019-08-03 23:30:43 +0800

    [Polygon] Not important code refactor.

--- src/Polygon/index.js
+++ src/Polygon/index.js
@@ -8,35 +8,30 @@ import isShallowEqual from '../utils/isShallowEqual';
 
 /**
  * Fields that need to be deep copied.
- * The new value is given to update api to avoid overwriting the props.
+ * AMap LBS library mutates options. Deep copy those options before passing to AMap so that
+ * props won't be mutated.
  */
 const NEED_DEEP_COPY_FIELDS = ['path'];
 
 /**
  * Polygon binding.
- * Polygon has the same config options as AMap.Polygon unless highlighted below.
- * For polygon events usage please reference to AMap.Polygon events paragraph.
+ * Polygon has the same options as AMap.Polygon unless highlighted below.
  * {@link http://lbs.amap.com/api/javascript-api/reference/overlay#polygon}
  */
 class Polygon extends React.Component {
-  /**
-   * AMap map instance.
-   */
-  static contextType = AMapContext;
-
   static propTypes = {
     /**
-     * Shows polygon by default, you can toggle show or hide by setting visible.
+     * Show polygon by default, you can toggle show or hide by changing visible.
      */
     visible: PropTypes.bool,
-    /* eslint-disable react/sort-prop-types,react/no-unused-prop-types */
     /**
      * Event callback.
-     *
-     * @param {AMap.Map} map           - AMap.Map instance
-     * @param {AMap.Polygon} polygon   - AMap.Polygon instance
-     * @param {Object} event           - Polygon event parameters
+     * Signature:
+     * (polygon, ...event) => void
+     * polygon: AMap.Polygon instance.
+     * event: AMap event.
      */
+    /* eslint-disable react/sort-prop-types,react/no-unused-prop-types */
     onComplete: PropTypes.func,
     onClick: PropTypes.func,
     onDblClick: PropTypes.func,
@@ -53,6 +48,15 @@ class Polygon extends React.Component {
     onTouchEnd: PropTypes.func,
     /* eslint-enable */
   };
+
+  static defaultProps = {
+    visible: true,
+  };
+
+  /**
+   * AMap map instance.
+   */
+  static contextType = AMapContext;
 
   /**
    * Parse AMap.Polygon options.
@@ -77,15 +81,13 @@ class Polygon extends React.Component {
       ...polygonOptions
     } = props;
 
-    return {
-      ...polygonOptions,
-    };
+    return polygonOptions;
   }
 
   /**
    * Define event name mapping relations of react binding Polygon and AMap.Polygon.
    * Initialise AMap.Polygon and bind events.
-   * Binding onComplete event on polygon instance.
+   * Fire complete action as soon as polygon has been created.
    */
   constructor(props, context) {
     super(props);
@@ -96,31 +98,27 @@ class Polygon extends React.Component {
 
     breakIfNotChildOfAMap('Polygon', map);
 
-    this.polygonOptions = Polygon.parsePolygonOptions(props);
+    this.polygonOptions = Polygon.parsePolygonOptions(this.props);
 
-    this.polygon = this.initPolygon(this.polygonOptions, map);
+    this.polygon = this.initPolygon(map);
 
-    this.eventCallbacks = this.parseEvents();
+    this.bindEvents();
 
-    this.bindEvents(this.polygon, this.eventCallbacks);
-
-    onComplete && onComplete(map, this.polygon);
+    typeof onComplete === 'function' && onComplete(map, this.polygon);
   }
 
   /**
    * Update this.polygon by calling AMap.Polygon methods.
-   * @param  {Object} nextProps
-   * @return {Boolean} - Prevent calling render function
    */
   shouldComponentUpdate(nextProps) {
     const nextPolygonOptions = Polygon.parsePolygonOptions(nextProps);
 
     const newPolygonOptions = cloneDeep(nextPolygonOptions, NEED_DEEP_COPY_FIELDS);
 
+    this.toggleVisible(this.polygonOptions.visible, nextPolygonOptions.visible);
+
     this.updatePolygonWithApi('setOptions', this.polygonOptions, nextPolygonOptions,
       newPolygonOptions);
-
-    this.toggleVisible(this.polygonOptions.visible, nextPolygonOptions.visible);
 
     this.polygonOptions = nextPolygonOptions;
 
@@ -141,23 +139,40 @@ class Polygon extends React.Component {
   }
 
   /**
-   * Initialise AMap polygon.
-   * @param {Object} polygonOptions - AMap.Polygon options
-   * @param {Object} map - Map instance
-   * @return {Polygon}
+   * Bind all events on map instance, and save event listeners which will be removed in
+   * componentWillUnmount lifecycle.
    */
-  initPolygon(polygonOptions, map) {
-    const polygon = new window.AMap.Polygon(
-      cloneDeep(
-        {
-          ...polygonOptions,
-          map,
-        },
-        NEED_DEEP_COPY_FIELDS,
-      ),
-    );
+  bindEvents() {
+    this.AMapEventListeners = [];
 
-    if (this.props.visible === false) polygon.hide();
+    /**
+     * Construct event callbacks.
+     */
+    const eventCallbacks = this.parseEvents();
+
+    Object.keys(eventCallbacks).forEach((key) => {
+      const eventName = key.substring(2).toLowerCase();
+      const handler = eventCallbacks[key];
+
+      this.AMapEventListeners.push(
+        window.AMap.event.addListener(this.polygon, eventName, handler),
+      );
+    });
+  }
+
+  /**
+   * Initialise AMap polygon.
+   */
+  initPolygon(map) {
+    const { visible } = this.props;
+
+    const newPolygonOptions = cloneDeep(this.polygonOptions, NEED_DEEP_COPY_FIELDS);
+
+    const polygon = new window.AMap.Polygon(newPolygonOptions);
+
+    polygon.setMap(map);
+
+    if (visible === false) polygon.hide();
 
     return polygon;
   }
@@ -184,52 +199,24 @@ class Polygon extends React.Component {
   }
 
   /**
-   * Bind all events on polygon instance.
-   * Save event listeners.
-   * Later to be removed in componentWillUnmount lifecycle.
-   * @param  {AMap.Polygon} polygon - AMap.Polygon instance
-   * @param  {Object} eventCallbacks - An object of all event callbacks
-   */
-  bindEvents(polygon, eventCallbacks) {
-    this.AMapEventListeners = [];
-
-    Object.keys(eventCallbacks).forEach((key) => {
-      const eventName = key.substring(2).toLowerCase();
-      const handler = eventCallbacks[key];
-
-      this.AMapEventListeners.push(
-        window.AMap.event.addListener(polygon, eventName, handler),
-      );
-    });
-  }
-
-  /**
-   * Update AMap.Polygon instance with named api and given value.
-   * Won't call api if the given value does not change.
-   * The new value is given to update api to avoid overwriting the props.
-   * @param  {string} apiName - AMap.Polygon instance update method name
-   * @param  {*} currentProp - Current value
-   * @param  {*} nextProp - Next value
-   * @param  {*} newProp - New value
-   */
-  updatePolygonWithApi(apiName, currentProp, nextProp, newProp) {
-    if (!isShallowEqual(currentProp, nextProp)) {
-      this.polygon[apiName](newProp);
-    }
-  }
-
-  /**
    * Hide or show polygon.
-   * @param  {Object} currentProp - Current value
-   * @param  {Object} nextProp - Next value
    */
-  toggleVisible(currentProp, nextProp) {
-    if (!isShallowEqual(currentProp, nextProp)) {
+  toggleVisible(previousProp, nextProp) {
+    if (!isShallowEqual(previousProp, nextProp)) {
       if (nextProp === true) this.polygon.show();
       if (nextProp === false) this.polygon.hide();
     }
   }
 
+  /**
+   * Update AMap.Polygon instance with named API and given value.
+   * Won't call API if the given value does not change.
+   */
+  updatePolygonWithApi(apiName, previousProp, nextProp, newProp) {
+    if (!isShallowEqual(previousProp, nextProp)) {
+      this.polygon[apiName](newProp);
+    }
+  }
 
   /**
    * Render nothing.
