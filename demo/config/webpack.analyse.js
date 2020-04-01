d13768230d19f93eebeab38b3d8181d48856a493
marcosun <marcoinbristol@gmail.com>
2020-04-01 09:49:08 +0800

    [core] Drop repository.

--- demo/config/webpack.analyse.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const dev = require('./webpack.dev.js');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-module.exports = merge.smart(dev, {
-  plugins: [
-    new BundleAnalyzerPlugin(),
-  ],
-});
--- src/utils/isNullVoid.js
+++
@@ -1,4 +0,0 @@
-export default function(parameter) {
-  if (parameter === void 0 || parameter === null) return true;
-  return false;
-}
